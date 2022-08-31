54f469cea5f6b318d55e966fcb2b3360d26b9032
Alex <brinzasalex@gmail.com>
2022-08-31 12:04:34 +0300

    Initial Commit

--- Homework18/src/main/java/org/fasttrackit/Homework18/Homework18Application.java
+++ Homework18/src/main/java/org/fasttrackit/Homework18/Homework18Application.java
@@ -1,16 +1,23 @@
 package org.fasttrackit.Homework18;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 
 @SpringBootApplication
 public class Homework18Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Homework18Application.class, args);
-
-
 	}
 
 
 }
+
